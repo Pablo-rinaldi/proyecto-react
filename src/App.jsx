ea2a2c8868cd79ec0ac46c7b1262b1f57fea936c
Pablo Rinaldi <pablohrinaldi@gmail.com>
2023-09-27 19:15:56 -0300

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { ItemListContainer } from "./components/ItemListContainer";
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <ItemListContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { CartWidget } from "./CartWidget";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <CartWidget />
+    </div>
+  );
+};
