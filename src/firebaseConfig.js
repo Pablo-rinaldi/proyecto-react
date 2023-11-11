656ae540e8678abd2d053248a8cabbf5ed946e51
Pablo Rinaldi <pablohrinaldi@gmail.com>
2023-11-11 18:44:08 -0300

    create .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeylHn7ewSTfuk8Ry55efwy5xqdfFwtCA",
-  authDomain: "kobb-d48a9.firebaseapp.com",
-  projectId: "kobb-d48a9",
-  storageBucket: "kobb-d48a9.appspot.com",
-  messagingSenderId: "762826084142",
-  appId: "1:762826084142:web:0eed12b7f562662fb8b4bf",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
